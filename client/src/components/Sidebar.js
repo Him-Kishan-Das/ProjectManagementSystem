93c75340c32869f52b202bb727e2c3c8d6caf28b
Him Kishan Das <70309670+Him-Kishan-Das@users.noreply.github.com>
2025-06-20 17:57:13 +0530

    Project Setup in MERN Stack

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () =>{
+    return (
+        <>
+            <h1>This is sidebar</h1>
+        </>
+    )
+}
+
+export default Sidebar;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+
+const Home = () =>{
+    return (
+        <>
+            <h1>This is working</h1>
+        </>
+    )
+}
+
+export default Home;
